288321ccf592f285f63ad3c58ad6d53b8b3931d3
Miquel Castro <castro-miquel@hotmail.com>
2021-03-31 18:17:35 +0200

    Segunda llamada a la API y formateo datos devue

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ program
 
 program.parse();
 
-console.log(`color: ${program.opts().abrev}`);
-
 inquirer.prompt([
   {
     type: "list",
@@ -47,17 +45,30 @@ inquirer.prompt([
       .then(res => {
         if (res.numberMatched === 0) {
           if (respuestasUsurio.errores) {
-            console.log(chalk.red.bold(`no existe la línea${respuestasUsurio.linea}`));
+            console.log(chalk.red.bold(`no existe la línea ${respuestasUsurio.linea}`));
             process.exit(0);
           } else {
             process.exit(0);
           }
         } else {
-          let colorConsola;
-          if (program.opts().color !== false) {
-            colorConsola = program.opts().color;
-          } else colorConsola = res.features[0].properties.COLOR_LINIA;
-          console.log(chalk.hex(colorConsola)(`${res.features[0].properties.NOM_LINIA} ${res.features[0].properties.DESC_LINIA}`));
+          const {
+            NOM_LINIA, COLOR_LINIA, DESC_LINIA, CODI_LINIA
+          } = res.features[0].properties;
+          const colorConsola = program.opts().color !== false ? program.opts().color : COLOR_LINIA;
+          console.log(chalk.hex(colorConsola)(`${NOM_LINIA} ${DESC_LINIA}`));
+          fetch(`https://api.tmb.cat/v1/transit/linies/metro/${CODI_LINIA}/estacions?app_id=a372a6d9&app_key=de3506372e19c90a75a39c1fa2dc9fb7`)
+            .then(res => res.json())
+            .then(objParadas => {
+              for (const parada of objParadas.features) {
+                const { NOM_ESTACIO } = parada.properties;
+                let paradaData = program.opts().abrev ? `${NOM_ESTACIO.slice(0, 3)}.` : NOM_ESTACIO;
+                paradaData += respuestasUsurio.informacion.includes("Fecha de inauguración")
+                  ? ` fecha inaguracion: ${parada.properties.DATA_INAUGURACIO}` : "";
+                paradaData += respuestasUsurio.informacion.includes("Coordenadas")
+                  ? ` cordenadas: ${parada.geometry.coordinates[0]} ${parada.geometry.coordinates[1]}` : "";
+                console.log(paradaData);
+              }
+            });
         }
       });
   });
